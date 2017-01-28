2e4a83b6a281a39bf85d23582e6a35433b5139a9
Ville Vaalisto <ville.vaalisto@gmail.com>
2017-01-28 20:47:30 +0200

    Panimolle näytetään arvioiden määrä ja keskiarvo

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,10 +1,15 @@
 class Brewery < ActiveRecord::Base
 	has_many :beers, dependent: :destroy
+	has_many :ratings, through: :beers
 
 	def print_report
 		puts name
 		puts "established at year #{year}"
 		puts "number of beers #{beers.count}"
+	end
+
+	def average_rating
+		return ratings.average(:score)
 	end
 
 	def restart
