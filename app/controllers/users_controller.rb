2c296be5f6d5097c52e7179e51fbf96c33afbff8
Ville Vaalisto <ville.vaalisto@gmail.com>
2017-02-04 23:45:16 +0200

    Käyttäjän poistaminen toimii oikein

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
-      if @user.update(user_params)
+      if @user.update(user_params) and @user == current_user
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
@@ -54,10 +54,13 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
+    if @user == current_user
+      @user.destroy
+      respond_to do |format|
+        format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+      session[:user_id] = nil
     end
   end
 
