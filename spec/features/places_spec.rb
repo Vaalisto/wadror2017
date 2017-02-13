573dae6aa40af483ee677c2e77dc44f688a36da2
Ville Vaalisto <ville.vaalisto@gmail.com>
2017-02-13 22:30:54 +0200

    Paikkojen hakutestit tehty

--- spec/features/places_spec.rb
+++ spec/features/places_spec.rb
@@ -12,4 +12,29 @@ describe "Places" do
 
     expect(page).to have_content "Oljenkorsi"
   end
+
+  it "if more than one are returned by the API, all are shown at the page" do
+  	places = [ Place.new( name:"Oljenkorsi", id:1),
+  				Place.new( name:"Kurvitar", id:2),
+  				Place.new( name:"Pääskypub", id:3) ]
+  	allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(places)
+
+  	visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    places.each do |place|
+    	expect(page).to have_content(place.name)
+    end
+  end
+
+  it "if no places are found, it is notified at the page" do
+  	allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return([])
+
+  	visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "No locations in kumpula"
+  end
 end
