5009fb2b0c5655f346642a4921c594633ba57861
Ville Vaalisto <ville.vaalisto@gmail.com>
2017-02-15 18:08:31 +0200

    Beermapping-testi korjattu

--- spec/lib/beermapping_api_spec.rb
+++ spec/lib/beermapping_api_spec.rb
@@ -26,6 +26,10 @@ describe "BeermappingApi" do
 
   describe "in case of cache hit" do
 
+    before :each do
+      Rails.cache.clear
+    end
+
     it "When one entry in cache, it is returned" do
       canned_answer = <<-END_OF_STRING
 <?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id>13307</id><name>O'Connell's Irish Bar</name><status>Beer Bar</status><reviewlink>http://beermapping.com/maps/reviews/reviews.php?locid=13307</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=13307&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=13307&amp;d=1&amp;type=norm</blogmap><street>Rautatienkatu 24</street><city>Tampere</city><state></state><zip>33100</zip><country>Finland</country><phone>35832227032</phone><overall>0</overall><imagecount>0</imagecount></location></bmp_locations>
@@ -40,8 +44,8 @@ describe "BeermappingApi" do
 
       expect(places.size).to eq(1)
       place = places.first
-      expect(place.name).to eq("Gallows Bird")
-      expect(place.street).to eq("Merituulentie 30")
+      expect(place.name).to eq("O'Connell's Irish Bar")
+      expect(place.street).to eq("Rautatienkatu 24")
     end
   end
 
