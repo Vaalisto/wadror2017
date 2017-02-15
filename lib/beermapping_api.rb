73f7b2fb5cb3310e57b28bbdc877d030c3e43c3c
Ville Vaalisto <ville.vaalisto@gmail.com>
2017-02-15 17:41:03 +0200

    Tehty välimuistitestit

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,7 +1,7 @@
 class BeermappingApi
   def self.places_in(city)
     city = city.downcase
-    Rails.cache.fetch(city) { fetch_places_in(city) }
+    Rails.cache.fetch(city, expires_in: 1.weeks) { fetch_places_in(city) }
   end
 
   private
