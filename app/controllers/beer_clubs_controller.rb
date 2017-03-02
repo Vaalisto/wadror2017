447b70ab00beab29e188fcaec3f43f41ae0cb421
Ville Vaalisto <ville.vaalisto@gmail.com>
2017-03-02 15:24:37 +0200

    Klubin tekijä liittyy automaattisesti vahvistettuna jäsenenä.

--- app/controllers/beer_clubs_controller.rb
+++ app/controllers/beer_clubs_controller.rb
@@ -35,7 +35,8 @@ class BeerClubsController < ApplicationController
     @beer_club = BeerClub.new(beer_club_params)
 
     respond_to do |format|
-      if @beer_club.save        
+      if @beer_club.save
+      	@beer_club.memberships.create user:current_user, confirmed:true
         format.html { redirect_to @beer_club, notice: 'Beer club was successfully created.' }
         format.json { render :show, status: :created, location: @beer_club }
       else
